8b56c1304f9168d5b02af6c4b9da169f36d025ea
Vivek Parmar <vivekparmar212023@gmail.com>
2026-07-04 23:09:36 +0530

    Navbar Complete

--- js/script.js
+++ js/script.js
@@ -2,13 +2,30 @@ const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
 const mobileBtnClose = document.querySelector(".close-menu-btn");
 const mobileMenu = document.querySelector(".navbar");
 const menuOverlay = document.querySelector(".menu-overlay");
+const navLinks = document.querySelectorAll(".nav-links a");
 
 mobileMenuBtn.addEventListener("click", () => {
   mobileMenu.classList.add("active");
   menuOverlay.classList.add("active");
+  document.body.classList.add("menu-open");
 });
 
 mobileBtnClose.addEventListener("click", () => {
   mobileMenu.classList.remove("active");
   menuOverlay.classList.remove("active");
+  document.body.classList.remove("menu-open");
+});
+
+menuOverlay.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+  menuOverlay.classList.remove("active");
+  document.body.classList.remove("menu-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    menuOverlay.classList.remove("active");
+    document.body.classList.remove("menu-open");
+  });
 });
