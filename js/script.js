08b5e547725856e28e2bba563068d415c41f6d1e
Vivek Parmar <vivekparmar212023@gmail.com>
2026-07-07 21:45:33 +0530

    Skill, Project, Contact Sections Complete

--- js/script.js
+++ js/script.js
@@ -4,28 +4,37 @@ const mobileMenu = document.querySelector(".navbar");
 const menuOverlay = document.querySelector(".menu-overlay");
 const navLinks = document.querySelectorAll(".nav-links a");
 
+/* Open mobile menu when clicking the menu button */
 mobileMenuBtn.addEventListener("click", () => {
   mobileMenu.classList.add("active");
   menuOverlay.classList.add("active");
   document.body.classList.add("menu-open");
 });
 
+/* Close mobile menu when clicking the close button */
 mobileBtnClose.addEventListener("click", () => {
   mobileMenu.classList.remove("active");
   menuOverlay.classList.remove("active");
   document.body.classList.remove("menu-open");
 });
 
+/* Close mobile menu when clicking outside */
 menuOverlay.addEventListener("click", () => {
   mobileMenu.classList.remove("active");
   menuOverlay.classList.remove("active");
   document.body.classList.remove("menu-open");
 });
 
+/* Reset mobile menu on link click */
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     mobileMenu.classList.remove("active");
     menuOverlay.classList.remove("active");
     document.body.classList.remove("menu-open");
   });
+});
+
+/* Reset contact form on page show */
+window.addEventListener("pageshow", () => {
+  document.querySelector(".contact-form").reset();
 });
