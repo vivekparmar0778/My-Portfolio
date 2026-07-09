cb313a318e6964983997e53aa443993fc8c3cc80
Vivek Parmar <vivekparmar212023@gmail.com>
2026-07-10 00:00:17 +0530

    animation done

--- js/script.js
+++ js/script.js
@@ -5,12 +5,20 @@ const menuOverlay = document.querySelector(".menu-overlay");
 const navLinks = document.querySelectorAll(".nav-links a");
 const header = document.querySelector(".header");
 
+/* bottom to top button */
+const scrollTopBtn = document.querySelector(".scroll-top-btn");
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 80) {
-    header.classList.add("scrolled");
+  if (window.scrollY > 400) {
+    scrollTopBtn.classList.add("show");
   } else {
-    header.classList.remove("scrolled");
+    scrollTopBtn.classList.remove("show");
   }
+});
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
 
 /* Open mobile menu when clicking the menu button */
@@ -46,4 +54,55 @@ navLinks.forEach((link) => {
 /* Reset contact form on page show */
 window.addEventListener("pageshow", () => {
   document.querySelector(".contact-form").reset();
+});
+
+/* for footer year update */
+const year = document.querySelector("#year");
+year.textContent = new Date().getFullYear();
+
+/* section highlight */
+const sections = document.querySelectorAll("section");
+window.addEventListener("scroll", () => {
+  // Navbar shadow
+  header.classList.toggle("scrolled", window.scrollY > 80);
+
+  // Active navigation
+  let currentSection = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.offsetHeight;
+
+    if (
+      window.scrollY >= sectionTop &&
+      window.scrollY < sectionTop + sectionHeight
+    ) {
+      currentSection = section.id;
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+});
+
+/* Reveal Animation */
+const revealElements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    } else {
+      entry.target.classList.remove("active");
+    }
+  });
+});
+
+revealElements.forEach((element) => {
+  observer.observe(element);
 });
